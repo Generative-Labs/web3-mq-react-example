1f0b09cd60e08c0b0f6484a8f8d17581b421cc49
zhaowei <zw204801@163.com>
2023-01-09 14:53:30 +0800

    feat: use  login modal

--- src/utils.tsx
+++
@@ -1,4 +0,0 @@
-export const getShortAddress = (address: string = '') => {
-  let strLength = address.length;
-  return address.substring(0, 5) + '...' + address.substring(strLength - 4, strLength);
-};
