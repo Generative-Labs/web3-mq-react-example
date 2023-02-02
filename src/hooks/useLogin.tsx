2ceddcd2f5bff5db3dd94a6c5b8b810dae2330b6
zhaowei <zw204801@163.com>
2023-02-02 18:12:31 +0800

    feat: update react components

--- src/hooks/useLogin.tsx
+++ src/hooks/useLogin.tsx
@@ -22,7 +22,6 @@ const useLogin = () => {
     const fastUrl = await Client.init({
       connectUrl: localStorage.getItem('FAST_URL'),
       app_key: 'vAUJTFXbBZRkEDRE',
-      env: 'dev',
       didKey,
       tempPubkey,
     });
