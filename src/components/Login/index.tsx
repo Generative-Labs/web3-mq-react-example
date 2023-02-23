802188abb4383f14d139b6a5d22252b402a605e1
zhaowei <zw204801@163.com>
2023-02-23 14:21:15 +0800

    feat: update react components

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,18 +1,23 @@
-import React, { useState } from 'react';
-import { Client, SignClientCallBackType } from '@web3mq/client';
-import {Modal, Loading, LoginModal, AppTypeEnum} from '@web3mq/react-components';
-import { Input, Select, message, Steps } from 'antd';
+import React, { useState } from "react";
+import { Client, SignClientCallBackType } from "@web3mq/client";
+import {
+  Modal,
+  Loading,
+  LoginModal,
+  AppTypeEnum,
+} from "@web3mq/react-components";
+import { Input, Select, message, Steps } from "antd";
 
-import useToggle from '../../hooks/useToggle';
-import { MetaMaskIcon } from '../../icons';
-import MPCBtn from '../MPCBtn';
+import useToggle from "../../hooks/useToggle";
+import { MetaMaskIcon } from "../../icons";
+import MPCBtn from "../MPCBtn";
 
-import './index.css';
+import "./index.css";
 
 interface IProps {
   setKeys: any;
   handleEvent: (options: SignClientCallBackType) => void;
-  appType?: AppTypeEnum
+  appType?: AppTypeEnum;
   mainKeys?: any;
   handleLoginEvent: any;
 }
@@ -22,11 +27,17 @@ const { Option } = Select;
 const { Step } = Steps;
 
 const Login: React.FC<IProps> = (props) => {
-  const {  setKeys, handleEvent, appType = AppTypeEnum.pc, handleLoginEvent, mainKeys = null } = props;
+  const {
+    setKeys,
+    handleEvent,
+    appType = AppTypeEnum.pc,
+    handleLoginEvent,
+    mainKeys = null,
+  } = props;
   const [step, setStep] = useState<number>(0);
-  const [didType, setDidType] = useState<string>('eth');
+  const [didType, setDidType] = useState<string>("eth");
   const [didValue, setDidValue] = useState<string>(
-    '0x7F96DDA344bDbb3747510eE7d0a1f88DD3E60Dc2'
+    "0x7F96DDA344bDbb3747510eE7d0a1f88DD3E60Dc2"
   );
   const [visible, show, hide] = useToggle();
   const [loading, showLoading, hideLoading] = useToggle();
@@ -41,20 +52,20 @@ const Login: React.FC<IProps> = (props) => {
 
   const signClient = async () => {
     if (!didType || !didValue) {
-      message.error('The value is require');
+      message.error("The value is require");
       return;
     }
     showLoading();
     const { data } = await fetch(
-      'https://dev-dapp-imdemo.web3mq.com/api/get_dapp_signature/',
+      "https://dev-dapp-imdemo.web3mq.com/api/get_dapp_signature/",
       {
-        method: 'POST',
-        mode: 'cors',
+        method: "POST",
+        mode: "cors",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          dapp_id: 'web3mq:imdemo',
+          dapp_id: "web3mq:imdemo",
           topic_id: `${didType}:${didValue}`,
           timestamp: Date.now(),
         }),
@@ -77,7 +88,7 @@ const Login: React.FC<IProps> = (props) => {
 
   const sendSignMsg = async () => {
     if (!didType || !didValue) {
-      message.error('The value is require');
+      message.error("The value is require");
       return;
     }
     showLoading();
@@ -92,100 +103,107 @@ const Login: React.FC<IProps> = (props) => {
   const ModalBtnGroup = () => {
     if (step === 0) {
       return (
-        <div className='ModalBtnGroupBtn' onClick={signClient}>
+        <div className="ModalBtnGroupBtn" onClick={signClient}>
           Temporary Connection
         </div>
       );
     }
     if (step === 1) {
       return (
-        <div className='ModalBtnGroupBtn' onClick={sendSignMsg}>
+        <div className="ModalBtnGroupBtn" onClick={sendSignMsg}>
           Send Validation Message
         </div>
       );
     }
     return (
-      <div className='tips'>
+      <div className="tips">
         Please verify on the phone and note keep the code is consistent
       </div>
     );
   };
 
   return (
-    <div className='login_container'>
-      <div className='step_box'>
-        <div className='up_text'>Welcome to Web3MQ</div>
-        <div className='down_text'>
+    <div className="login_container">
+      <div className="step_box">
+        <div className="up_text">Welcome to Web3MQ</div>
+        <div className="down_text">
           Let’s get started with your decentralized trip now!
         </div>
-        <div className='step_text'>Step1: Connect Wallet</div>
+        <div className="step_text">Step1: Connect Wallet</div>
       </div>
-      <div className='button_box'>
+      <div className="button_box">
         <LoginModal
-            env={'dev'}
-            containerId={''}
-            keys={mainKeys}
-            handleLoginEvent={handleLoginEvent}
-            appType={appType}
-            loginBtnNode={
-              <button className="sign_btn">
-                <MetaMaskIcon />
-                MetaMask
-              </button>
-            }
+          env={"dev"}
+          containerId={""}
+          keys={mainKeys}
+          handleLoginEvent={handleLoginEvent}
+          appType={appType}
+          styles={{
+            addressBox: {
+              width: "281px",
+            },
+          }}
+          loginBtnNode={
+            <button className="sign_btn">
+              <MetaMaskIcon />
+              MetaMask
+            </button>
+          }
         />
         {/*<button onClick={show} className='sign_btn'>*/}
         {/*  <img className='btnIcon' src='./web3mq.logo.ico' alt='' />*/}
         {/*  <span>Phone verification</span>*/}
         {/*</button>*/}
         <Modal
-          title='Phone verification'
+          title="Phone verification"
           visible={visible}
           closeModal={() => {
             hide();
             setStep(0);
           }}
-          dialogClassName='modalContent'>
-          <div className='step'>
-            <Steps size='small' current={step}>
-              <Step title='Create connect' />
-              <Step title='Send message' />
-              <Step title='Confirm' />
+          dialogClassName="modalContent"
+        >
+          <div className="step">
+            <Steps size="small" current={step}>
+              <Step title="Create connect" />
+              <Step title="Send message" />
+              <Step title="Confirm" />
             </Steps>
           </div>
           {loading && (
-            <div className='modalLoading'>
+            <div className="modalLoading">
               <Loading />
             </div>
           )}
           {step === 0 && !loading && (
-            <div className='modalContentBody modalContentBodyInput'>
+            <div className="modalContentBody modalContentBodyInput">
               <Select
-                size='large'
-                style={{ width: '60%', height: 42 }}
+                size="large"
+                style={{ width: "60%", height: 42 }}
                 value={didType}
-                onChange={handleSelect}>
-                <Option value='eth'>Eth</Option>
-                <Option value='lens.xyz'>Lens</Option>
+                onChange={handleSelect}
+              >
+                <Option value="eth">Eth</Option>
+                <Option value="lens.xyz">Lens</Option>
               </Select>
               <br />
               <Input
                 value={didValue}
                 onChange={handleInput}
-                placeholder='please enter your Did Value'
+                placeholder="please enter your Did Value"
               />
             </div>
           )}
           {step === 1 && !loading && (
-            <div className='modalContentBody modalContentBodyInput'>
+            <div className="modalContentBody modalContentBodyInput">
               Step2: Send Message
             </div>
           )}
           {step === 2 && !loading && (
-            <div className='modalContentBody codeContainer'>
-              <div className='code'>
-                {Client.signClient.tempCode.split('').map((item, idx) => (
-                  <span key={idx} className='codeItem'>
+            <div className="modalContentBody codeContainer">
+              <div className="code">
+                {Client.signClient.tempCode.split("").map((item, idx) => (
+                  <span key={idx} className="codeItem">
                     {item}
                   </span>
                 ))}
